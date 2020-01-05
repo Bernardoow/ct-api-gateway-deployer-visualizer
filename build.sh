4e2020bd15daeba92190a6a6f83db1dc93ffee9c
Bernardo Gomes <bgomesdeabreu@gmail.com>
2020-01-05 11:58:43 -0300

    Added settings to automated deploy.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+js="elm.js"
+min="elm.min.js"
+
+./node_modules/.bin/elm make --optimize --output=$js ./src/Main.elm
+./node_modules/.bin/uglifyjs $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | ./node_modules/.bin/uglifyjs --mangle --output=./gh-pages/$min
+rm $js
